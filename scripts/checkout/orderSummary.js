5b7e71fd13115783557b6a09578bb50598f7b057
Rahul <sanapathirahul123@gmail.com>
2024-03-29 20:04:05 +0530

    Save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,9 +1,9 @@
 import {cart, removeFromCart,updateDeliveryOption} from '../../data/cart.js'
-import {products} from '../../data/products.js';
+import {products,getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { deliveryOptions } from '../../data/delivaryoptions.js';
+import { deliveryOptions,getDeliveryOption } from '../../data/deliveryoptions.js';
 hello();
 const today=dayjs();
 const deliveryDate=today.add(7,'days');
@@ -16,15 +16,16 @@ export function renderOrderSummary(){
   cart.forEach((cartItem) => {
     const productId = cartItem.productId;
 
-    let matchingProduct;
+    let matchingProduct=getProduct(productId);
 
     products.forEach((product) => {
       if (product.id === productId) {
+
         matchingProduct = product;
       }
     });
     const deliveryOptionId=cartItem.deliveryOptionId;
-    let deliveryOption;
+    let deliveryOption=getDeliveryOption(deliveryOptionId);
     deliveryOptions.forEach((option)=>{
       if (option.id===deliveryOptionId){
         deliveryOption=option;
@@ -75,32 +76,34 @@ export function renderOrderSummary(){
     `;
   });
   function deliveryOptionsHTML(matchingProduct,cartItem){
+
     let html='';
     deliveryOptions.forEach((deliveryOption)=>{
-      const today=dayjs();
-      const deliveryDate=today.add(deliveryOption.deliveryDays,'days');
-      const dateString=deliveryDate.format('dddd, MMMM D');
-      const priceString=deliveryOption.priceCents===0
-      ?'FREE'
-      :`$${formatCurrency(deliveryOption.priceCents)}- `;
-      const isChecked=deliveryOption.id===cartItem.deliveryOptionId;
+    const today=dayjs();  
+    const deliveryDate=today.add(deliveryOption.deliveryDays,'days');
+    const dateString=deliveryDate.format('dddd, MMMM D');
+    const priceString=deliveryOption.priceCents===0
+    ?'FREE'
+    :`$${formatCurrency(deliveryOption.priceCents)}- `;
+    const isChecked=deliveryOption.id===cartItem.deliveryOptionId;
 
-      html+=
-      `
-    <div             class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}"
-      data-delivery-option-id="${deliveryOption.id}">
-      <input type="radio"
-      ${isChecked ? 'checked' :''}
-        class="delivery-option-input"
-        name="delivery-option-${matchingProduct.id}">
-      <div>
-        <div class="delivery-option-date">
-          ${dateString}
+    html+=`
+
+    <div class="delivery-option js-delivery-option" 
+        data-product-id="${matchingProduct.id}"
+        data-delivery-option-id="${deliveryOption.id}">
+        <input type="radio"
+            ${isChecked ? 'checked' :''}
+            class="delivery-option-input"
+            name="delivery-option-${matchingProduct.id}">
+        <div>
+            <div class="delivery-option-date">
+                ${dateString}
+            </div>
+            <div class="delivery-option-price">
+                ${priceString} - Shipping
+            </div>
         </div>
-        <div class="delivery-option-price">
-          ${priceString} - Shipping
-        </div>
-      </div>
     </div>
     `
     });
@@ -122,11 +125,13 @@ export function renderOrderSummary(){
         container.remove();
       });
     });
-  document.querySelector('.js-delivery-option').forEach((element)=>{
+
+document.querySelectorAll('.js-delivery-option').forEach((element)=>{
     element.addEventListener('click',()=>{
-      const {productId,deliveryOptionId}=element.dataset;
-      updateDeliveryOption(productId,deliveryOptionId);
-      renderOrderSummary();
+
+        const{productId,deliveryOptionId}=element.dataset;
+        updateDeliveryOption(productId,deliveryOptionId);
+        
+        });
     });
-  });
 }
